7b2b692bd938bb0cdad651b67bb5fde1a195a065
Thefirstxerg <thefirstxerg@gmail.com>
2025-02-26 14:48:55 +0200

    Update

--- Book1/Chapter6/dictionaries.py
+++ Book1/Chapter6/dictionaries.py
@@ -1,57 +1,58 @@
-# # Dictionaries/Objects
+# Dictionaries/Objects
 
-# alien_0 = {'color': 'green', 'points': 5}
-# print(alien_0['color'])
-# print(alien_0['points'])
-
-
-# # Accessing Values in a Dictionary
-# alien_0 = {'color': 'green'}
-# print(alien_0['color'])
-# # print(alien_0['points'])  # KeyError: 'points'
+alien_0 = {'color': 'green', 'points': 5} # A dictionary is a collection of key-value pairs. Each key is connected to a value, 
+                                          # and you can use a key to access the value associated with that key.
+print(alien_0['color'])
+print(alien_0['points'])
 
 
-# # Adding and removing New Key-Value Pairs
-# alien_0 = {'color': 'green', 'points': 5}
-# print(alien_0)
-# alien_0['x_position'] = 0
-# alien_0['y_position'] = 25
-# print(alien_0)
-# del alien_0['points']
-# print(alien_0)
+# Accessing Values in a Dictionary
+alien_0 = {'color': 'green'}
+print(alien_0['color'])
+# print(alien_0['points'])  # KeyError: You cannot access a key that does not exist in the dictionary.
 
 
-# # Modifying Values in a Dictionary
-# alien_0 = {'color': 'green'}
-# print(f"The alien is {alien_0['color']}.")
-# alien_0['color'] = 'yellow'
-# print(f"The alien is now {alien_0['color']}.")
-# alien_0['color'] = 'red'
-# print(f"The alien is now {alien_0['color']}.")
-# alien_0['color'] = 'blue'
-# print(f"The alien is now {alien_0['color']}.")
+# Adding and removing New Key-Value Pairs
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+del alien_0['points'] # The del statement removes the key-value pair from the dictionary.
+print(alien_0)
 
 
-# # A Dictionary of Similar Objects
-# favorite_languages = {
-#     'jen': 'python',
-#     'sarah': 'c',
-#     'edward': 'ruby',
-#     'phil': 'python',
-# }
-# print(f"Sarah's favorite language is {favorite_languages['sarah'].title()}.")
-# print(f"Phil's favorite language is {favorite_languages['phil'].title()}.")
-# print(f"Edward's favorite language is {favorite_languages['edward'].title()}.")
-# print(f"Jen's favorite language is {favorite_languages['jen'].title()}.")
-# print()
+# Modifying Values in a Dictionary
+alien_0 = {'color': 'green'}
+print(f"The alien is {alien_0['color']}.")
+alien_0['color'] = 'yellow' # Modifying the value of the key color.
+print(f"The alien is now {alien_0['color']}.")
+alien_0['color'] = 'red'
+print(f"The alien is now {alien_0['color']}.")
+alien_0['color'] = 'blue'
+print(f"The alien is now {alien_0['color']}.")
 
 
-# # Using get() to Access Values
-# alien_0 = {'color': 'green', 'speed': 'slow'}
-# print(alien_0.get('color'))
+# A Dictionary of Similar Objects
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+} 
+print(f"Sarah's favorite language is {favorite_languages['sarah'].title()}.") # The title() method capitalizes the first letter of each
+print(f"Phil's favorite language is {favorite_languages['phil'].title()}.")   # word in the string.
+print(f"Edward's favorite language is {favorite_languages['edward'].title()}.")
+print(f"Jen's favorite language is {favorite_languages['jen'].title()}.")
+print()
 
-# point_value = alien_0.get('points', 'No point value assigned.') # WHen the key is not found in the dictionary, the second argument is returned.
-# print(point_value)
+
+# Using get() to Access Values
+alien_0 = {'color': 'green', 'speed': 'slow'}
+print(alien_0.get('color'))
+
+point_value = alien_0.get('points', 'No point value assigned.') # WHen the key is not found in the dictionary, the second argument is returned.
+print(point_value)
 
 
 
@@ -63,13 +64,13 @@ alien_0 = {'color': 'green',
            'x_position': 0,
            'y_position': 25}
 
-for key, value in alien_0.items():
-    print(f"\n{key}: {value}")
-    if key == 'color':
+for key, value in alien_0.items(): # The items() method returns a list of key-value pairs, 
+    print(f"\n{key}: {value}")     # key variable is assigned to the key and value variable is assigned to the value.
+    if key == 'color': # Checking if the key is color, then print the color value of the alien.
         print(f"\nThe alien is {value}.")
     elif key == 'points':
         print(f"The alien has {value} points.")
-    elif key == 'x_position':
+    elif key == 'x_position': # Checking if the key is x_position, modify the x_position, then print the x_position value of the alien.
         value = alien_0['x_position']
         print(f"The alien is now at position {value}.")
     elif key == 'y_position':
@@ -85,5 +86,6 @@ son = {'name': 'Son', 'age': 20, 'hometown': 'Pretoria'}
 daughter = {'name': 'Daughter', 'age': 15, 'hometown': 'Pretoria'}
 
 family = [dad, mom, son, daughter]
-for member in family:
+
+for member in family: # Looping through the list of dictionaries and printing the key-value pairs.
     print(f"{member}")
--- Book1/Chapter7/inputs_while.py
+++ Book1/Chapter7/inputs_while.py
@@ -1,11 +1,12 @@
 # Inputs and while Loops 
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
-
+message = input("Tell me something, and I will repeat it back to you: ") # input() function pauses the program and waits for the user
+print(message)                                                           # to enter some text. Once the user presses Enter, the input() function
+                                                                         # passes that input back to the program. The input is stored in the 
+                                                                         # variable message.
 name = input("Please enter your name: ")
 print("Hello, " + name + "!")
 age = input("How old are you? ")
-age = int(age)
+age = int(age) # The int() function converts a string representation of a number to a numerical representation. The same goes for str(), float(), etc.
 if age >= 18:
     print("You are an adult.")
 else:
@@ -24,12 +25,25 @@ else:
 
 # While Loops
 current_number = 1
-while current_number <= 5:
+while current_number <= 5: # The while loop runs as long as the condition is true
     print(current_number)
-    current_number += 1
+    current_number += 1 # Increments the value of current_number by 1 until it reaches 5
 
+
+# Own Example: Health Slowly drains until 0 and then prints "You are dead."
 health = 100
 while health > 0:
     print("You are still alive.")
     health -= 10
 print("You are dead.")
+
+
+# Letting the user choose when to quit
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit': # The loop runs as long as the value of message is not 'quit'
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+
