3b13ee10ef7205b36196eb642331a529a320d7e4
Thefirstxerg <thefirstxerg@gmail.com>
2025-03-04 14:47:28 +0200

    Upload

---
+++ Book1/Chapter10/Storing_data.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import json
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+path = Path("numbers.json")
+with open(path, "w") as file: # w = write
+    json.dump(number, file) # Dump is used to write the data to the file
+
+with open(path, "r") as file: # r = read
+    numbers = json.load(file) # Load is used to read the data from the file
+
+
