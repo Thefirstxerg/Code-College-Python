3b67bab135a39b129753475a010586a164add939
Thefirstxerg <thefirstxerg@gmail.com>
2025-03-10 10:27:30 +0200

    Update mpl_squares.py

--- Book1/Chapter15/mpl_squares.py
+++ Book1/Chapter15/mpl_squares.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
+# Coordinates
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3,color='red')
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
