4d95f9acaa166a782fc0a24be8a855897681a07f
Thefirstxerg <thefirstxerg@gmail.com>
2025-03-14 14:05:57 +0200

    Upload

--- Book1/Chapter18/learning_log/views.py
+++ Book1/Chapter18/learning_log/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def index(request):
-    """The home page for Learning Log."""
-    return render(request, 'learning_log/index.html')
+
+ return render(request, 'C:\Users\ulric\OneDrive\Documents\Code College\Web bootcamp\Python\Code\Book1\Chapter18\learning_log\templates\learning_logs\index.html')
